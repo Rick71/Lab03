528a665f46e925e4a76abb89bb3a54733b50feaa
Ricardo Vera <ricardo@uag.com>
2015-02-03 23:59:25 -0600

    Initial Commit

---
+++ Agendalab03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Agendalab03
+//
+//  Created by Ricardo Vera on 03/02/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Agendalab03/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Agendalab03
+//
+//  Created by Ricardo Vera on 03/02/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
