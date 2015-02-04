1a201a9c3221baed08134a4d81bcb18e8afe6640
Ricardo Vera <ricardo@uag.com>
2015-02-04 01:30:43 -0600

    vistas creadas s

---
+++ Agendalab03/Detalle.h
@@ -0,0 +1,13 @@
+//
+//  Detalle.h
+//  Agendalab03
+//
+//  Created by Ricardo Vera on 04/02/15.
+//  Copyright (c) 2015 Comercomp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detalle : UIViewController
+
+@end
